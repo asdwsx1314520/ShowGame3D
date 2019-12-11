bf01d0ad2a28770649d8e8c678ea256b32284074
asdwsx1314520 <40228509+asdwsx1314520@users.noreply.github.com>
2019-12-11 21:30:23 +0800

    3D

---
+++ UnityProject/Assets/Script/MenuControler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class MenuControler : MonoBehaviour
+{
+    public Animator ani_ui;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void button_mall()
+    {
+
+    }
+
+    public void button_equipment()
+    {
+
+    }
+
+    public void button_talent()
+    {
+
+    }
+
+    public void button_levels()
+    {
+
+    }
+}
