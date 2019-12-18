baf5ccb3727afee98ff2c617c692e29b8cf214d1
asdwsx1314520 <40228509+asdwsx1314520@users.noreply.github.com>
2019-12-18 21:34:48 +0800

    虛擬搖桿腳本

---
+++ UnityProject/Assets/Script/play.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class play : MonoBehaviour
+{
+    public Rigidbody rig;
+    public Joystick joysitck;
+    public float speed;
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        float h = joysitck.Horizontal;
+        float v = joysitck.Vertical;
+
+        rig.AddForce(-h * speed, 0, -v * speed);
+    }
+}
