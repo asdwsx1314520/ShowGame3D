f40aa2c081378400d585def54d88e5951d706313
asdwsx1314520 <40228509+asdwsx1314520@users.noreply.github.com>
2020-02-21 21:30:30 +0800

    準備發布

---
+++ UnityProject/Assets/Script/MenuManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("玩家資料")]
+    public PlayerData data;
+
+    public void BuyHp1000()
+    {
+        data.hpMax += 1000;
+        data.hp = data.hpMax;
+    }
+
+    public void StartGame()
+    {
+        data.hp = data.hpMax;
+        SceneManager.LoadScene(1);
+    }
+}
